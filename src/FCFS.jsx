f87d10f67d196d5fa8b4e8d0c29862ce86af68b0
SOHEL RANA <sohelrana104322@gmail.com>
2025-04-24 09:48:19 +0600

    change file name

--- src/FCFS.jsx
+++ src/FCFS.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function App() {
+function FCFS() {
   const [processes, setProcesses] = useState([]);
   const [newProcess, setNewProcess] = useState({
     id: "",
@@ -123,4 +123,4 @@ function App() {
   );
 }
 
-export default App;
+export default FCFS;
